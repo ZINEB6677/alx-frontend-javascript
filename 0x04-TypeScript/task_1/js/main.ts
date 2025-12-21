72166a79d749e34398e4439808266bcd268ecfa6
ZINEB6677 <136370303+ZINEB6677@users.noreply.github.com>
2025-12-21 19:52:13 +0200

    h

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,25 @@ const teacher2: Teacher = {
 };
 
 console.log(teacher1);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+  contract: true,
+};
+const director2: Directors = {
+  firstName: 'david',
+  lastName: 'miller',
+  location: 'New York',
+  fullTimeEmployee: true,
+  numberOfReports: 7,
+  contract: false,
+};
+
+console.log(director1);
